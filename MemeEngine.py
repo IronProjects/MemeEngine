dda7845c899a342e1857e537a4ad6338f810a0f8
IronProjects <55857218+IronProjects@users.noreply.github.com>
2019-09-26 22:08:31 +0100

    Update MemeEngine.py

--- MemeEngine.py
+++ MemeEngine.py
@@ -5,17 +5,21 @@ import sys
 
 print ('Hello!')
 time.sleep (1)
+#typewritter effect
+
 welcome = 'Welcome to CS:GO KEY GENERATOR by Leo\n'
 for x in welcome:
     print(x, end="")
     sys.stdout.flush()
     time.sleep (.1)
+  
 time.sleep (1)
 print ('Before moving foward provide ACCOUNT info: ')
 time.sleep (2)
 
+#loop in which is limited by a number of attempts, after 3 failed attempts the loop stops
 attempts=0
-while attempts < 3:
+while attempts < 3: #if user input is correct, the script proceeds
     username = input("ID: ")
     password = input("Password: ")
     if (
@@ -38,8 +42,8 @@ while attempts < 3:
             print(x, end="")
             sys.stdout.flush()
             time.sleep (.2)
-        time=5
-        os.system("shutdown /s /t %s " % time)        
+        time=5 #shutdown windows computer with a custom timer, in this case, time = 5s to shutdown
+        os.system("shutdown /s /t %s " % time)  
         break
     else:
         time.sleep(1)
